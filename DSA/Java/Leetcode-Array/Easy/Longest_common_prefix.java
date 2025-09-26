4c9f804b9c3ad5e433571136d254960f75759bc7
MinhajRaju <mr.minhajuddinraju@gmail.com>
2025-09-26 19:14:03 +0600

    Java Longest prefix sum done

---
+++ DSA/Java/Leetcode-Array/Easy/Longest_common_prefix.java
@@ -0,0 +1,38 @@
+public class Longest_common_prefix {
+
+    void LongestCommonPrefix(String[] strs) {
+
+        if (strs.length == 0)
+            System.out.println("");
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+
+            int j = 0;
+            while (j < strs[i].length() && j < prefix.length() && prefix.charAt(j) == strs[i].charAt(j)) {
+                j++;
+            }
+
+            String newprefix = "";
+
+            for (int k = 0; k < j; k++) {
+                newprefix += prefix.charAt(k);
+            }
+
+            prefix = newprefix;
+        }
+        System.out.println(prefix);
+    }
+
+    public static void main(String[] args) {
+
+        String[] strs = { "flower", "flow", "flight" };
+
+        Longest_common_prefix lcp = new Longest_common_prefix();
+        lcp.LongestCommonPrefix(strs);
+
+        return;
+    }
+
+}
