6453d550f3b4ef79a5adf2dc67a26c3c74557751
MinhajRaju <mr.minhajuddinraju@gmail.com>
2025-10-07 17:41:47 +0600

    IslandPreimeter done

---
+++ DSA/C++/Leetcode-Array/Easy/Find_max_consecutive_one.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+
+
+void MaxConsecutiveOne(std::vector<int>& nums){
+
+    int n  = nums.size();
+
+    for(int i=0; i< n; i++){
+
+        for (int j = 0;  j < n; j++){
+
+
+        }
+
+    }
+
+
+
+}
+
+
+int main(){
+
+    std::vector<int> nums = {1,1,0,1,1,1};
+
+
+
+
+    return 0;
+}
