e01f5a9659cd841787a834cc5e8e330b2ca9e86c
MinhajRaju <mr.minhajuddinraju@gmail.com>
2025-10-10 17:26:00 +0600

    Array partition done

---
+++ DSA/C++/Leetcode-Array/Easy/Array_partition.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void ArrayPairSum(std::vector<int>& nums){
+
+    sort(nums.begin() , nums.end());
+    int  sum = 0;
+    for(int i = 0 ; i< nums.size() ; i+=2){
+        std::cout << i;
+        sum +=nums[i];
+    }
+
+    std::cout << sum;
+
+}
+
+int main(){
+
+    std::vector<int> nums = {6,2,6,5,1,2};
+    ArrayPairSum(nums);
+    return 0;
+}
+
+
+
+
+
+
+
