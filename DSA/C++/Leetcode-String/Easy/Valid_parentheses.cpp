fef173d9d26c770cd721dc26c72480da048c96d3
MinhajRaju <mr.minhajuddinraju@gmail.com>
2025-10-11 10:18:32 +0600

    Vali parentheses done

---
+++ DSA/C++/Leetcode-String/Easy/Valid_parentheses.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+bool ValidParentheses(std::string s){
+
+    std::stack<char> st;
+
+    for(int i  = 0;  i < s.size(); i++){
+
+        char  ch = s[i];
+
+        if(ch == '(' || ch == '{' || ch == '['){
+            st.push(ch);
+        }else{
+            if(st.empty()) return false;
+
+            char top = st.top();
+
+            if( (ch == ')' && top != '(') 
+             ||  (ch == '}' && top != '{') 
+             ||              (ch == ']' && top != '[')){
+
+                return false;
+            }
+
+
+           st.pop();
+        }
+
+
+    }
+
+   return st.empty();
+   
+}
+
+
+int main(){
+    std::string s = "([])";
+    bool res  = ValidParentheses(s);
+    std::cout << res;
+    return 0;
+}
