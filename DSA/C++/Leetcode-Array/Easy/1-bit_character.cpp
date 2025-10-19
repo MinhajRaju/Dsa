2fd40662466f215ffb717f508cf96c660217497b
MinhajRaju <mr.minhajuddinraju@gmail.com>
2025-10-19 11:18:54 +0600

    1 bit charchae done

---
+++ DSA/C++/Leetcode-Array/Easy/1-bit_character.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+
+bool Bits(std::vector<int>& bits){
+
+    int n = bits.size();
+
+    int i = 0;
+
+    while( i < n -1){
+        if(bits[i] == 1)  i +=2;
+        else i +=1;
+    }
+    return i == n-1;
+
+
+  
+
+
+};
+
+
+
+
+int main(){
+
+    std::vector<int> bits = {1,1,1,0};
+
+    bool res = Bits(bits);
+    std::cout << res;
+    return 0;
+
+
+
+}
