ec7e9b60ed9659f7e67830c78b7825df1e7a4fa5
MinhajRaju <mr.minhazuddinrazu@gmail.com>
2026-04-01 16:35:49 +0600

    sum of odd done

---
+++ DSA/C++/Leetcode-Array/Easy/Sum_of_odd.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+int SumOfOdd(std::vector<int>& nums){
+
+
+    int n  = nums.size();
+    int totalSum = 0;
+
+
+    for(int i = 0 ; i < n ; i ++){
+
+        int currnetSubArraySum = 0;
+        for (int j  = i ; j < n ; j++){
+            currnetSubArraySum += nums[j];
+            int length = j-i+1;
+
+            if (length % 2 != 0){
+                totalSum += currnetSubArraySum;
+            }
+        }
+
+
+    }
+
+
+    return  totalSum;
+
+}
+
+
+
+
+int main(){
+
+
+    std::vector<int> nums = {1, 4, 2, 5, 3};
+
+
+
+    std::cout  << SumOfOdd(nums);
+
+
+    return 0;
+}
